8acbc9e11573b34cee85cc77b3e29f86d3699357
Uzo-I-O <uzomaoduah@gmail.com>
2024-07-07 18:50:42 +0100

    Third Question

---
+++ DrawingCanvas/DrawingCanvas/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MouseDrawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlCanvas
+            // 
+            this.pnlCanvas.BackColor = System.Drawing.Color.White;
+            this.pnlCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCanvas.Location = new System.Drawing.Point(26, 12);
+            this.pnlCanvas.Name = "pnlCanvas";
+            this.pnlCanvas.Size = new System.Drawing.Size(558, 274);
+            this.pnlCanvas.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 315);
+            this.Controls.Add(this.pnlCanvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCanvas;
+    }
+}
+
---
+++ DrawingCanvas/DrawingCanvas/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseDrawing
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point lastPoint;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pnlCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            lastPoint = e.Location;
+        }
+
+        private void pnlCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = pnlCanvas.CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, lastPoint, e.Location);
+                    lastPoint = e.Location;
+                }
+            }
+        }
+
+        private void pnlCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+    }
+}
